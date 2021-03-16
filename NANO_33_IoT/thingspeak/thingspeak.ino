37e432ec45a8dec98edd2c45df11c73f731d03dc
JerryJack121 <73692359+JerryJack121@users.noreply.github.com>
2021-03-16 20:57:39 +0800

    Update

--- NANO_33_IoT/thingspeak/thingspeak.ino
+++ NANO_33_IoT/thingspeak/thingspeak.ino
@@ -1,7 +1,7 @@
-//#include <ESP8266WiFi.h>
-//#include <WiFiClient.h>
 #include <SPI.h>
 #include <WiFiNINA.h>
+#include <Arduino.h>
+#include "wiring_private.h"
 
 
 // 設定網路基地台SSID跟密碼
@@ -16,16 +16,33 @@ const char* resource = "/update?api_key=";
 // Thing Speak API server 
 const char* server = "api.thingspeak.com";
 
+// 設定虛擬序列埠
+Uart pHSerial (&sercom0, 5, 6, SERCOM_RX_PAD_1, UART_TX_PAD_0);
+int DE_RE = 3;  // 致能腳位
+
+// 設定虛擬序列埠
+void SERCOM0_Handler()
+{
+    pHSerial.IrqHandler();
+}
+
+// 函數宣告
+float getpH(String pH); 
+float command(unsigned char item[]);
 
 void setup() {
   // Initializing serial port for debugging purposes
+  pinPeripheral(5, PIO_SERCOM_ALT);
+  pinPeripheral(6, PIO_SERCOM_ALT);
   Serial.begin(9600);
-  delay(10);
+  pHSerial.begin(9600);
+  pinMode(DE_RE, OUTPUT);
+  //  digitalWrite(DE_RE,LOW);    
+  while (!Serial) ; // 等待序列埠連線
+  Serial.println("序列埠連線完成");
 
-  Serial.println("");
-  Serial.println("");
-  Serial.print("Connecting To: ");
-  Serial.println(ssid);
+  Serial.println();
+  Serial.println();
 
 
   // 等待連線，並從 Console顯示 IP
@@ -53,28 +70,138 @@ void setup() {
 
 void loop() {
   
-  int data = 50;
+  // 命令字串
+  unsigned char item1[] = {0x01, 0x04, 0x00, 0x00, 0x00, 0x02, 0x71, 0xCB};
+  unsigned char item2[] = {0x02, 0x04, 0x00, 0x00, 0x00, 0x02, 0x71, 0xF8};
+  unsigned char item3[] = {0x03, 0x04, 0x00, 0x00, 0x00, 0x02, 0x70, 0x29};
+  unsigned char item4[] = {0x04, 0x04, 0x00, 0x06, 0x00, 0x02, 0x91, 0x9F};
+
+  float ph, con, do_, orp;
   
-  Serial.println(data);
+  Serial.println("PH值");
+  ph = command(item1);
+  delay(2000);
+  
+  Serial.println("鹽度CON");
+  con = command(item2);
+  delay(2000);
+  Serial.println("DO溶氧量");
+  do_ = command(item3);
+  delay(2000);
+  Serial.println("ORP電極");
+  orp = command(item4);
+  delay(2000);
 
 //  連線到server
   Serial.print("Connecting to "); 
   Serial.print(server);
   WiFiClient client;
-   // 使用 80 Port 連線
-//  if (client.connect(server, 80)) {
-//    Serial.println(F("connected"));
-//  } 
-//  else  {
-//    Serial.println(F("connection failed"));
-//    return;
-//  }
-//
-//
-//  client.print(String("GET ") + resource + apiKey + "&field1=" + data +
-//                  " HTTP/1.1\r\n" +
-//                  "Host: " + server + "\r\n" + 
-//                  "Connection: close\r\n\r\n");
-//  delay(15000); // thingspeak每筆資料間隔至少15秒
+// 使用 80 Port 連線
+  if (client.connect(server, 80)) {
+    Serial.println(F("connected"));
+  } 
+  else  {
+    Serial.println(F("connection failed"));
+    return;
+  }
+// 上傳thingspeak
+  client.print(String("GET ") + resource + apiKey + "&field1=" + ph + "&field2=" + con + "&field3=" + do_ + "&field4=" + orp +
+                  " HTTP/1.1\r\n" +
+                  "Host: " + server + "\r\n" + 
+                  "Connection: close\r\n\r\n");
+  delay(15000); // thingspeak每筆資料間隔至少15秒
   
   }
+
+
+// 傳送指令給感測器，並呼叫getdata解讀回傳值
+float command(unsigned char item[]){
+    String ans = "";
+    unsigned long runTime1, runTime2;
+    int count = 0;
+    
+    Serial.println("---------------");
+    //高電位-寫入模式
+    digitalWrite(DE_RE, HIGH);
+    Serial.println("寫入模式");
+
+    // 傳送測溫命令
+    for (int i = 0; i < 8; i++)
+    {                     
+        pHSerial.write(item[i]); // 依序寫入指令
+        Serial.print(item[i], HEX);
+        delay(10) ;
+    }
+    Serial.println();
+    
+    //高電位-讀取模式
+    digitalWrite(DE_RE, LOW);
+    Serial.println("讀取模式");
+   
+     runTime1 = millis();      //讀取 Arduino 板執行時間
+     
+  // 等待感測器回應，若太久沒有回應則退出
+    while(!pHSerial.available())
+    {
+      runTime2 = millis();
+//      Serial.println(runTime2-runTime1);
+      if((runTime2-runTime1)>= 3000)
+        break;
+    }
+    // 讀取感測器回傳指令
+    while (pHSerial.available()) 
+    { 
+        count += 1;
+        unsigned char in = (unsigned char)pHSerial.read(); 
+//        Serial.print(in, HEX);
+        ans += in;
+        ans += ',';
+        delay(10);
+    }
+    Serial.println(ans);
+    // 判斷感測器是否有回應   
+    if (count != 0){
+      if (count!=(5+2*(int(item[5])))) // 若回傳長度不對，則回傳錯誤
+      {
+        Serial.println("回傳長度錯誤");
+        return -1.0;
+      }
+      else{
+            Serial.println("解讀數值");
+           float info = getdata(ans);
+            Serial.println(info);
+            return info;
+          }
+    }
+    else
+    {
+      Serial.println("感測器無回應");
+      return -1.0;
+    }
+    
+  }
+
+//解讀回傳指令
+float getdata(String ans)
+{
+    int commaPosition;
+    String info[9]; // 用字串陣列儲存
+    for (int i = 0; i < 11; i++)
+    {
+        commaPosition = ans.indexOf(','); // 尋找字串中逗號的位置
+        if (commaPosition != -1)
+        {
+            info[i] = ans.substring(0, commaPosition);
+            ans = ans.substring(commaPosition + 1, ans.length());
+        }
+    }
+    if (info[2].toInt() == 2)
+      return (info[3].toInt() * 256 + info[4].toInt() * 16);
+    else if (info[2].toInt() == 4){
+      Serial.print("小數點位數:");
+      Serial.println(info[5].toInt());
+      Serial.print("單位代號:");
+      Serial.println(info[6].toInt(),HEX);
+      return ((info[3].toInt() * 256 + info[4].toInt())/pow(10, info[5].toInt()));
+      }
+}
